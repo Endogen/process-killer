fc10ce29de5b28dc1248eb2a8be83ac30c494d0b
endogen <081081@gmx.net>
2023-11-08 23:24:51 +0100

    Fixed some bugs

--- killer.py
+++ killer.py
@@ -2,23 +2,20 @@ import psutil
 import flet as ft
 from flet_core import KeyboardEvent
 
-
 # TODO: How to save and load window position?
 # TODO: Window resizes on startup
 # TODO: Display shortcuts in background
+
+processes = dict()
+
+for proc in psutil.process_iter():
+    processes[proc.name()] = proc
+
+p_keys = list(processes.keys())
+
+
 def main(page: ft.Page):
-    processes = dict()
-    p_keys = list()
-
-    def get_process_list():
-        processes.clear()
-        for proc in psutil.process_iter():
-            processes[proc.name()] = proc
-
-        p_keys = list(processes.keys())  # FIXME: Why is that not recognized?
-
-    get_process_list()
-    print(p_keys)  # TODO: Remove
+    global p_keys
 
     def kill_process(e):
         if e.control in list_view.controls:
@@ -27,6 +24,10 @@ def main(page: ft.Page):
             if p_name in p_keys:
                 try:
                     processes[p_name].kill()
+                    del processes[p_name]
+                    p_keys.remove(p_name)
+                    list_view.controls.remove(e.control)
+                except psutil.NoSuchProcess:
                     del processes[p_name]
                     p_keys.remove(p_name)
                     list_view.controls.remove(e.control)
@@ -56,6 +57,8 @@ def main(page: ft.Page):
     list_view = ft.ListView(expand=1, spacing=10, padding=20)
 
     def on_keyboard(e: KeyboardEvent):
+        global p_keys
+
         # ESC - Clear input field
         if e.key == "Escape":
             text_field.value = None
@@ -64,7 +67,12 @@ def main(page: ft.Page):
 
         # CTRL + K - Multi kill
         elif e.key == "K" and e.control:
+            tmp_list = list()
+
             for entry in list_view.controls:
+                tmp_list.append(entry)
+
+            for entry in tmp_list:
                 p_name = entry.title.value
 
                 if p_name in p_keys:
@@ -73,15 +81,29 @@ def main(page: ft.Page):
                         del processes[p_name]
                         p_keys.remove(p_name)
                         list_view.controls.remove(entry)
+                    except psutil.NoSuchProcess:
+                        del processes[p_name]
+                        p_keys.remove(p_name)
+                        list_view.controls.remove(entry)
                     except psutil.AccessDenied:
                         entry.leading = ft.Icon(ft.icons.ERROR)
+
+            if not list_view.controls:
+                text_field.value = None
 
             text_field.focus()
             page.update()
 
         # CTRL + R - Refresh process list
         elif e.key == "R" and e.control:
-            get_process_list()
+            text_field.value = None
+            list_view.controls = None
+
+            processes.clear()
+            for process in psutil.process_iter():
+                processes[process.name()] = process
+
+            p_keys = list(processes.keys())
 
         # CTRL + Q - Exit app
         elif e.key == "Q" and e.control:
